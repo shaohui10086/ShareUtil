a485b9e0b560b8977af181d102b830b99bb91ffb
Pitt <qifengsheng@heylotus.com>
2017-03-14 18:10:26 +0800

    add user_id,remove openId for facebookToken.

--- shareutil/src/main/java/me/shaohui/shareutil/login/result/FbToken.java
+++ shareutil/src/main/java/me/shaohui/shareutil/login/result/FbToken.java
@@ -7,12 +7,20 @@ import com.facebook.AccessToken;
  */
 
 public class FbToken extends BaseToken {
+    private String user_id;
 
     public static FbToken parse(AccessToken token) {
         final FbToken fbToken = new FbToken();
         fbToken.setAccessToken(token.getToken());
-        //TODO add OpenId
-        fbToken.setOpenid(token.getUserId());
+        fbToken.setUserId(token.getUserId());
         return fbToken;
+    }
+
+    public String getUserId() {
+        return user_id;
+    }
+
+    public void setUserId(String userId) {
+        this.user_id = userId;
     }
 }
